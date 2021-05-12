86a0e1e6440b3c78a539452748c5c5d7795bf592
MohcinBN <rapmabiz@gmail.com>
2021-05-12 02:54:32 +0000

    Working on home section

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,30 @@
 <template>
-  <div id="home" class="section">
-    TEEEEEEEst
+  <div
+    id="home"
+    class="section"
+    :style="{
+      'background-image': 'url(' + require('@/assets/images/bg-2.jpg') + ')',
+      'background-size': 'cover',
+      'background-position': 'center',
+      'background-repeat': 'no-repeat',
+      filter: 'brightness(0.6)',
+    }"
+  >
+    <main class="content-wrapper">
+      <div class="welcoming">
+        <h1>Assalamo Alaykom</h1>
+        <h2>I'm</h2>
+        <h3>Mohcin.</h3>
+        <span>Software Developer</span>
+      </div>
+      <aside class="personal-image">
+        <img
+          src="@/assets/images/image.jpg"
+          alt="my personal image"
+          class="w-100"
+        />
+      </aside>
+    </main>
   </div>
 </template>
 
@@ -14,4 +38,49 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 77vw !important;
+  padding-top: 8%;
+  color: #fff;
+}
+.personal-image img {
+  width: 300px;
+  border: 3px solid #fff;
+  border-radius: 11px;
+  transition: 0.3s ease-in;
+}
+.personal-image img:hover {
+  transform: scale(1.2);
+}
+.welcoming h1,
+.welcoming h2 {
+  font-weight: 300;
+  font-size: 3rem;
+  line-height: 21px;
+}
+.welcoming h3 {
+  font-weight: bold;
+  font-size: 4rem;
+  line-height: 0;
+}
+.welcoming span {
+  background: #fff;
+  color: #222;
+  width: 100%;
+  display: block;
+  text-align: center;
+  padding: 11px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: 0.3s ease-in;
+  font-size: 2rem;
+}
+.welcoming span:hover {
+  background: #527751;
+  color: #fff;
+}
+</style>
